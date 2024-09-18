4dca60c01fac52761e1965dd2aaedee336a45832
K-Chris-Nicholas-Vachan-Deep <123354197+K-Chris-Nicholas-Vachan-Deep@users.noreply.github.com>
2024-09-18 12:59:13 +0530

    canny.py

--- canny.py
+++ canny.py
@@ -1,25 +1,5 @@
-import cv2
-import matplotlib.pyplot as plt
-
-# Read the image in grayscale
-image = cv2.imread(r"/Users/chrisnikky/Downloads/helo.jpeg", cv2.IMREAD_GRAYSCALE)
-
-# Check if the image is loaded correctly
-if image is None:
-    print("Error: Could not open or find the image.")
-else:
-    # Apply Canny edge detection
-    edges = cv2.Canny(image, 100, 200)  # 100 and 200 are the lower and upper thresholds for edge detection
-
-    # Display the original image and the edge-detected result
-    plt.figure(figsize=(10,5))
-
-    plt.subplot(1, 2, 1)
-    plt.imshow(image, cmap='gray')
-    plt.title('Original Image')
-
-    plt.subplot(1, 2, 2)
-    plt.imshow(edges, cmap='gray')
-    plt.title('Canny Edge Detection')
-
-    plt.show()
+import cv2, matplotlib.pyplot as plt
+img = cv2.imread(r'C:\Users\deves\Downloads\CV IMAGES\images.jpeg', 0)
+canny = cv2.Canny(img, 100, 200)
+plt.imshow(canny, cmap='gray')
+plt.show()
